4408d04ba0e9259a34d899c330362e9b630f69ee
dilaraacetin <dilaracetin13@gmail.com>
2025-12-15 22:42:29 +0300

    removed event models and services

--- src/main/java/com/example/icslog/application/port/in/IngestEventPort.java
+++
@@ -1,7 +0,0 @@
-package com.example.icslog.application.port.in;
-
-import com.example.icslog.domain.model.Event;
-
-public interface IngestEventPort {
-    void ingestEvent(Event event);
-}
--- src/main/java/com/example/icslog/application/port/out/EventStore.java
+++
@@ -1,7 +0,0 @@
-package com.example.icslog.application.port.out;
-
-import com.example.icslog.domain.model.Event;
-
-public interface EventStore {
-    Event save(Event event);
-}
